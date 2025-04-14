3fbf209522dda5c939d0998e3e9db3c561ca4207
Flavien Charlon <flavien@charlon.org>
2025-04-14 23:49:43 +0200

    Configure Maven publishing

--- jobqueue/build.gradle.kts
+++ jobqueue/build.gradle.kts
@@ -11,3 +11,43 @@ dependencies {
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            pom {
+                name = "jobqueue"
+                description = "jobqueue is a concurrency management library for Kotlin."
+                url = "https://github.com/flavien/jobqueue"
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+                developers {
+                    developer {
+                        name = "Flavien Charlon"
+                        email = "flavien@charlon.org"
+                    }
+                }
+                scm {
+                    connection = "scm:git:git://github.com/flavien/jobqueue.git"
+                    url = "https://github.com/flavien/jobqueue/tree/master"
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri(layout.buildDirectory.dir("../../publish/${project.name}-${project.version}"))
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["maven"])
+}
